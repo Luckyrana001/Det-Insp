986c83a1bd7b1a996a419042814fa4cd802f3f95
user.luckyrana <luckyrana321@gmail.com>
2014-09-08 14:01:50 +0530

    commit test third

--- GitTest/src/com/example/gittest/MainActivity.java
+++ GitTest/src/com/example/gittest/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		Log.i("its working","its  cool");
+		Log.i("its working properly","its  cool");
 
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
@@ -38,7 +38,7 @@ public class MainActivity extends ActionBarActivity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
+		// as youv specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
